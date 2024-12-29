3c179399cf43d5937c5c564992f557736c068042
Bruno Walalce de Freitas Franca <brunowff@hotmail.com>
2024-12-29 17:35:39 -0300

    feat: Creation of basic Group Service methods

--- src/main/java/br/com/doubletelecom/help_desk_tickets/app/services/GroupServices.java
+++ src/main/java/br/com/doubletelecom/help_desk_tickets/app/services/GroupServices.java
@@ -1,5 +1,17 @@
 package br.com.doubletelecom.help_desk_tickets.app.services;
 
+import java.util.List;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import br.com.doubletelecom.help_desk_tickets.app.domain.dtos.CreateGroupDto;
+import br.com.doubletelecom.help_desk_tickets.app.domain.entities.Group;
+
 public interface GroupServices {
+    Group save(CreateGroupDto groupDto, JwtAuthenticationToken token);
+    Group findById(String groupId);
+    List<Group> findAll();
+    Group update(CreateGroupDto groupDto, JwtAuthenticationToken token);
+    Void delete(String groupId, JwtAuthenticationToken token);
 
 }
--- src/main/java/br/com/doubletelecom/help_desk_tickets/app/services/implementations/GroupServiceImpl.java
+++ src/main/java/br/com/doubletelecom/help_desk_tickets/app/services/implementations/GroupServiceImpl.java
@@ -1,7 +1,36 @@
 package br.com.doubletelecom.help_desk_tickets.app.services.implementations;
 
+import java.util.List;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import br.com.doubletelecom.help_desk_tickets.app.domain.dtos.CreateGroupDto;
+import br.com.doubletelecom.help_desk_tickets.app.domain.entities.Group;
 import br.com.doubletelecom.help_desk_tickets.app.services.GroupServices;
+import jakarta.transaction.Transactional;
 
 public class GroupServiceImpl implements GroupServices{
+    
+    @Override
+    @Transactional
+    public Group save(CreateGroupDto groupDto, JwtAuthenticationToken token) {
+        return null;
+    }
+
+    public Group findById(String groupId) {
+        return null;
+    }
+    
+    public List<Group> findAll() {
+        return null;
+    }
+
+    public Group update(CreateGroupDto groupDto, JwtAuthenticationToken token) {
+        return null;
+    }
+
+    public Void delete(String groupId, JwtAuthenticationToken token) {
+        return null;
+    }
 
 }
