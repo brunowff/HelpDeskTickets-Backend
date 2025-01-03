1abb4069b4c4faa79c0e61cbc194ff3bee431c52
Bruno Wallace de Freitas Franca <brunowff@himalayan.metrotelecom.com.br>
2025-01-03 16:48:34 -0300

    fix: change the default roles creation names in DB by .sql script file

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -9,8 +9,8 @@ INSERT INTO tb_roles (role_id, name) VALUES (7, 'API_ROLE') ON CONFLICT (role_id
 INSERT INTO tb_roles (role_id, name) VALUES (8, 'API_ROLE_MANAGER') ON CONFLICT (role_id) DO NOTHING;
 INSERT INTO tb_roles (role_id, name) VALUES (9, 'API_TICKET') ON CONFLICT (role_id) DO NOTHING;
 INSERT INTO tb_roles (role_id, name) VALUES (10, 'API_TICKET_MANAGER') ON CONFLICT (role_id) DO NOTHING;
-INSERT INTO tb_roles (role_id, name) VALUES (11, 'API_TICKET_TYPE') ON CONFLICT (role_id) DO NOTHING;
-INSERT INTO tb_roles (role_id, name) VALUES (12, 'API_TICKET_TYPE_MANAGER') ON CONFLICT (role_id) DO NOTHING;
+INSERT INTO tb_roles (role_id, name) VALUES (11, 'API_TICKET_CATEGORY') ON CONFLICT (role_id) DO NOTHING;
+INSERT INTO tb_roles (role_id, name) VALUES (12, 'API_TICKET_CATEGORY_MANAGER') ON CONFLICT (role_id) DO NOTHING;
 INSERT INTO tb_roles (role_id, name) VALUES (13, 'API_TICKET_MESSAGE') ON CONFLICT (role_id) DO NOTHING;
 INSERT INTO tb_roles (role_id, name) VALUES (14, 'API_TICKET_MESSAGE_MANAGER') ON CONFLICT (role_id) DO NOTHING;
 INSERT INTO tb_roles (role_id, name) VALUES (15, 'API_TICKET_LOG') ON CONFLICT (role_id) DO NOTHING;
